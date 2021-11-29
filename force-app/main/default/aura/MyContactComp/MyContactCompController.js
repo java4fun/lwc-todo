5f7288523cc4c88510afc639c789dd404ad2b036
java4fun <perl4fun@gmail.com>
2021-11-28 21:29:58 -0800

    Validation - custom validation

--- force-app/main/default/aura/MyContactComp/MyContactCompController.js
+++ force-app/main/default/aura/MyContactComp/MyContactCompController.js
@@ -27,7 +27,27 @@
             btn.set('v.name', 'save');
             btn.set('v.label', 'Save');
         } else if (name == 'save') {
-            helper.saveContacts(component, event, helper);
+            var contactFields = component.find("fieldToValidate");
+            var blank = 0;
+            if (contactFields.length != undefined) {
+                var allValid = contactFields.reduce(function(validSoFar, inputCmp) {
+                    inputCmp.showHelpMessageIfInvalid();
+                    // 
+                    return validSoFar && inputCmp.get('v.validity').valid;
+                }, true);
+
+                if (!allValid) {
+                    blank++;
+                }
+            } else {
+                var allValid = contactFields;
+                if (!allValid.get('v.validity').valid) {
+                    black++;
+                }
+            }
+            if (blank == 0) {
+                helper.saveContacts(component, event, helper);
+            }
         }
     },
 
@@ -50,6 +70,26 @@
     },
 
     createContact : function(component, event, helper) {
+        var isContactValid = component.validateContact(component, event, helper);
         helper.insertContact(component, event, helper);
+    },
+
+    validateContact : function(component, event, helper) {
+        var allValid = component.find("formFieldToValidate").reduce(function(validSoFar, inputCmp) {
+            
+            inputCmp.showHelpMessageIfInvalid();
+
+            var name = inputCmp.get("v.name");
+            if(name == "emailField") {
+                var value = inputCmp.get("v.value");
+                if (value != 'rahul@gmail.com') {
+                    inputCmp.focus();
+                    inputCmp.set('v.validity', {valid: false, badInput: true});
+                }
+            }
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+
+        return allValid;
     }
 })
